54ceb1ff16212386bc9bbace66437a07c723f7d2
boltechz <akinolaboluwatife52@gmail.com>
2022-03-22 14:38:09 -0700

    Correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
 while (*(s + count) != '\0')
 {
-if (size > 0 && (*(s + count) < '0' || *(s + count) > '8'))
+if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
 if (*(s + count) == '-')
 pn *= -1;
