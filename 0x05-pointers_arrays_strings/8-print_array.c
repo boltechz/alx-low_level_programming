795f44300b0121f9acf4ec5fabd9699f648f4319
boltechz <akinolaboluwatife52@gmail.com>
2022-03-22 14:17:48 -0700

    Print Arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,14 +7,14 @@
  * @n: input n elements
  * Return: no return.
  */
-void print_array(int *a, int *n)
+void print_array(int *a, int n)
 {
 int i = 0;
 
 for (; i < n; i++)
 {
 printf("%d", *(a + i));
-if (i != (n -1))
+if (i != (n - 1))
 printf(", ");
 }
 printf("\n");
